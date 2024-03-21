6c23d64ab86906726aa8f4fbe4f6cc7dce4a3de9
jbaxax <walterjave92@gmail.com>
2024-03-21 16:03:09 -0500

    maps

---
+++ game.js
@@ -0,0 +1,61 @@
+const canvas = document.querySelector('#game');
+const game = canvas.getContext('2d');
+
+let canvasSize;
+let elementsSize;
+
+window.addEventListener('load', setCanvasSize);
+window.addEventListener('resize', setCanvasSize)
+function starGame() {
+
+
+
+    console.log({ canvasSize, elementsSize });
+
+    game.font = elementsSize + 'px Verdana';
+    game.textAlign = 'end'
+
+    const map = maps[0];
+    const mapRows = map.trim().split('\n');
+    const mapRowCols = mapRows.map(row=>row.trim().split(''))
+
+    
+    console.log(map,mapRows,mapRowCols);
+
+    mapRowCols.forEach((row,rowI) => {
+        row.forEach((col,colI)=>{
+            const emoji =emojis[col];
+            const posX = elementsSize*(colI+1);
+            const posY = elementsSize*(rowI+1);
+                
+            
+            game.fillText(emoji,posX,posY)
+        })
+    });
+
+    /* for (let row = 1    ; row <= 10; row++) {
+        for (let col = 1; col <= 10; col++) {
+            game.fillText(emojis[mapRowCols[row-1][col-1]], elementsSize * col, elementsSize * row);
+
+        }
+
+    } */
+
+
+}
+
+function setCanvasSize() {
+
+
+    if (window.innerHeight > window.innerWidth) {
+        canvasSize = window.innerWidth * 0.8;
+    } else {
+        canvasSize = window.innerHeight * 0.8;
+    }
+    canvas.setAttribute('width', canvasSize)
+    canvas.setAttribute('height', canvasSize)
+
+    elementsSize = canvasSize / 10;
+
+    starGame()
+}
